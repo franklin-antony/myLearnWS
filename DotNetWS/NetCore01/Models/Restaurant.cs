80335ed2c7a7ccb2624952a8802425a11f207100
franklin-antony <36724845+franklin-antony@users.noreply.github.com>
2018-03-16 23:26:32 +0400

    MVC controller in action

---
+++ DotNetWS/NetCore01/Models/Restaurant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore01.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+    }
+}
