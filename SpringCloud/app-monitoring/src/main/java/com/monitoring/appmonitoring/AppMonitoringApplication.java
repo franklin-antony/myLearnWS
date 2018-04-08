55e63e40b2995d9eff3cfddb54ce96394bf154f3
Franklin Antony <Franklin.Antony@emaratech.ae>
2018-04-08 13:12:16 +0400

    Circuit Breaker with Netflix Hystrix

--- SpringCloud/app-monitoring/src/main/java/com/monitoring/appmonitoring/AppMonitoringApplication.java
+++ SpringCloud/app-monitoring/src/main/java/com/monitoring/appmonitoring/AppMonitoringApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@SpringBootApplication
 @EnableHystrixDashboard
+@SpringBootApplication
 public class AppMonitoringApplication {
 
 	public static void main(String[] args) {
